90cc7dfadbc917bc145e5b3d8994ec9c1efad0e4
buroudo <51899923+buroudo@users.noreply.github.com>
2021-03-31 21:13:40 +0900

    Add files via upload

--- sample.js
+++ sample.js
@@ -1,5 +1,10 @@
 var URLHEAD = "https://line.me/R/msg/text/?";
 var INDENT = "%0D%0A";
+var REPORT = ['gotoReport_', 'arrivalReport_' , 'backHomeReport_']
+var TABLE_TITLE = ['日付', '出発時上番報告' , '到着時上番報告','下番報告']
+
+//変数storageにlocalStorageを格納
+var storage = localStorage;
 
 //グローバル定数を定義する
 function define(name, value){
@@ -29,11 +34,16 @@ function buttonClick() {
 //送信日の作成
 function nowDay(){
     const date1 = new Date();
-    const date2 = date1.getFullYear() + "年" + 
-				(date1.getMonth() + 1)  + "月" + 
-				date1.getDate() + "日"  ;
+    const date2 = createDayStr(date1.getFullYear(),(date1.getMonth() + 1) ,date1.getDate());
 
     return date2;
+}
+
+//送信日の文章作成
+function createDayStr(year,month,day){
+    const dayStr = year + "年" + month  + "月" + day + "日"  ;
+
+    return dayStr;
 }
 
 //送信時の作成
@@ -49,31 +59,32 @@ function nowTime(){
 //試験用　出発上番報告
 function gotoReport() {
     var sendStr = URLHEAD + "%E3%81%8A%E3%81%AF%E3%82%88%E3%81%86%E3%81%94%E3%81%96%E3%81%84%E3%81%BE%E3%81%99%E3%80%82%0D%0A%E3%81%93%E3%82%8C%E3%82%88%E3%82%8A%E7%8F%BE%E5%A0%B4%E3%81%AB%E5%90%91%E3%81%8B%E3%81%84%E3%81%BE%E3%81%99%E3%80%82";
-    saveReport( "gotoReport_" + nowDay() + "=" + nowTime());
+    saveReport( REPORT[0] + nowDay() , nowTime());
     window.open(sendStr, '_blank');
 }
 
 //試験用　到着上番報告
 function arrivalReport() {
     var sendStr = URLHEAD + "%E7%8F%BE%E5%A0%B4%E3%81%AB%E5%88%B0%E7%9D%80%E3%81%97%E3%81%BE%E3%81%97%E3%81%9F%E3%80%82%0D%0A%E6%A5%AD%E5%8B%99%E9%96%8B%E5%A7%8B%E3%81%BE%E3%81%A7%E5%BE%85%E6%A9%9F%E3%81%97%E3%81%BE%E3%81%99%E3%80%82";
-    saveReport( "arrivalReport_" + nowDay() + "=" + nowTime());
+    saveReport( REPORT[1] + nowDay() , nowTime());
     window.open(sendStr, '_blank');
 }
 
 //試験用　下番報告
 function backHomeReport() {
     var sendStr = URLHEAD + "%E6%A5%AD%E5%8B%99%E7%B5%82%E4%BA%86%E3%81%97%E3%81%BE%E3%81%97%E3%81%9F%E3%80%82%0D%0A%E3%81%93%E3%82%8C%E3%82%88%E3%82%8A%E7%9B%B4%E5%B8%B0%E3%81%97%E3%81%BE%E3%81%99%E3%80%82";
-    saveReport( "backHomeReport_" + nowDay() + "=" + nowTime());
+    saveReport( REPORT[2] + nowDay() , nowTime());
     window.open(sendStr, '_blank');
 }
 
-//クッキー上に報告時間の保存
-//文字の並びは【報告種類：日時 = 報告時間】
-//タグは報告種類：日時となる。
-function saveReport(saveStr){
-    console.log(saveStr);
-    document.cookie = saveStr;
-    console.log(document.cookie);
+//ストレージ上に報告時間の保存
+//文字の並びは【報告種類：日時 , 報告時間】
+//キーは報告種類：日時となる。
+function saveReport(keyDay,time){
+
+    //keyDayというキーにtimeという値を割り当てて保存
+    storage.setItem(keyDay, time);
+
 }
 
 //テーブル作成
@@ -81,33 +92,66 @@ function generate_table() {
     // get the reference for the body
     var body = document.getElementsByTagName("body")[0];
   
+    //古いテーブルがあった場合は削除する。
+    var oldTable = document.getElementById("reportTable");
+    if(oldTable != null){
+        oldTable.remove();
+    }
+
     // creates a <table> element and a <tbody> element
     var tbl = document.createElement("table");
+    tbl.id = "reportTable";
     var tblBody = document.createElement("tbody");
-  
-    var tmp = document.cookie;
-    var cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)gotoReport_2021年3月14日\s*\=\s*([^;]*).*$)|^.*$/, "$1");
-    console.log(cookieValue);
+
+    //タイトルの作成
+    {
+        var row = document.createElement("tr");
+        for(var i = 0; i < TABLE_TITLE.length; i++){
+            var cell = document.createElement("td");
+            cell.appendChild(document.createTextNode(TABLE_TITLE[i]));
+            row.appendChild(cell);
+        }
+        tblBody.appendChild(row);
+    }
+    //指定月の日数を取得する。
+    const date = new Date();
+    var monthDayCount = getLastDay(date.getFullYear(),(date.getMonth() + 1));
+    
+
     // creating all cells
-    for (var i = 0; i < 2; i++) {
-      // creates a table row
-      var row = document.createElement("tr");
-  
-      //列方向
-      for (var j = 0; j < 3; j++) {
-        // Create a <td> element and a text node, make the text
-        // node the contents of the <td>, and put the <td> at
-        // the end of the table row
-        var cell = document.createElement("td");
-        
-        //セル内の文字列設定
-        //クッキーの内容を一行分
-        
-        var cellText = document.createTextNode(cookieValue);
-        cell.appendChild(cellText);
-        row.appendChild(cell);
-      }
-  
+    for (var i = 0; i < monthDayCount; i++) {
+        // creates a table row
+        var row = document.createElement("tr");
+
+        //日付行
+        var dateCell = document.createElement("td");
+        rawDayStr = createDayStr(date.getFullYear(),(date.getMonth() + 1),i+1);
+        dateCellStr = document.createTextNode(rawDayStr);
+        dateCell.appendChild(dateCellStr);
+        row.appendChild(dateCell);
+        //報告状況
+        for (var j = 0; j < 3; j++) {
+            // Create a <td> element and a text node, make the text
+            // node the contents of the <td>, and put the <td> at
+            // the end of the table row
+            var cell = document.createElement("td");
+            
+            //セル内の文字列設定
+            //ストレージの内容を一行分
+            var k = REPORT[j] + rawDayStr;
+            var cellText = document.createTextNode("取得ミス");
+            value = storage.getItem(k);
+            if( value == null ){
+                cell.style.color = "#CC6666";
+                cellText = document.createTextNode("報告無し");
+            }else{
+                var result = value;
+                cellText = document.createTextNode(result);
+            }
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+        }
+    
       // add the row to the end of the table body
       tblBody.appendChild(row);
     }
@@ -119,3 +163,13 @@ function generate_table() {
     // sets the border attribute of tbl to 2;
     tbl.setAttribute("border", "2");
   }
+
+ /**
+ * 指定月の日数を取得
+ * @param  {number} year  年
+ * @param  {number} month 月
+ * @return {number} 指定月の日数
+ */
+const getLastDay = (year, month) => {
+    return new Date(year, month, 0).getDate();
+};
